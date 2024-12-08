8f638daa7523feaa74ba70f396ee693a05864490
alanaasmar <asmarala@msu.edu>
2024-12-08 08:47:01 +0000

    shop complete

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const navMenu = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+
+const tabs = document.querySelectorAll('#tabs .tab');
+const mainImage = document.getElementById('current-image');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const newSrc = tab.getAttribute('data-src');
+        mainImage.src = newSrc;
+
+        tabs.forEach(tab => tab.classList.remove('active'));
+        tab.classList.add('active');
+    });
+});
+
+tabs[0].classList.add('active');
